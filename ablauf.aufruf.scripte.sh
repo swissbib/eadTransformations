e8e02d06c26e84f25817b00668374468a05c4a3b
Oliver Schihin <oliver.schihin@unibas.ch>
2015-06-23 14:28:58 +0200

    PATH-unabhängiger Aufruf, gitignore-Anpassung

--- ablauf.aufruf.scripte.sh
+++ ablauf.aufruf.scripte.sh
@@ -3,7 +3,7 @@
 basedir=$PWD
 
 
-#transform.ha2marc.sh $basedir CHARCH01
-remove.marc.namespaces.sh $basedir
-transform.into.1.line.sh $basedir
+$basedir/transform.ha2marc.sh $basedir CHARCH01
+$basedir/remove.marc.namespaces.sh $basedir
+$basedir/transform.into.1.line.sh $basedir
 
